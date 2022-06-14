7ce3c90369f733127f9c82ddfe28cde5b41fca83
Neutralization <50076677+Neutralization@users.noreply.github.com>
2022-06-15 02:02:53 +0800

    change default IP

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,29 +11,29 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-mkdir -p files/etc/uci-defaults
-cat << "EOF" > files/etc/uci-defaults/90_custom
-uci -q batch << EOI
-set network.lan.ipaddr='192.168.2.1'
-set network.@device[2].macaddr='b8:27:eb:48:f8:30'
-set network.@device[2].ipv6='0'
-del network.wan6
-commit network
-set dhcp.lan.force='1'
-set dhcp.lan.ra_flags='none'
-del dhcp.lan.ra
-del dhcp.lan.ra_slaac
-del dhcp.lan.dhcpv6
-commit dhcp
-set system.@system[0]=system
-set system.@system[0].hostname='CN210101841'
-set system.@system[0].timezone='CST-8'
-set system.@system[0].zonename='Asia/Shanghai'
-set system.@system[0].ttylogin='0'
-set system.@system[0].log_size='64'
-set system.@system[0].log_proto='udp'
-set system.@system[0].urandom_seed='0'
-commit system
-EOF
+# mkdir -p files/etc/uci-defaults
+# cat << "EOF" > files/etc/uci-defaults/90_custom
+# uci -q batch << EOI
+# set network.lan.ipaddr='192.168.2.1'
+# set network.@device[2].macaddr='b8:27:eb:48:f8:30'
+# set network.@device[2].ipv6='0'
+# del network.wan6
+# commit network
+# set dhcp.lan.force='1'
+# set dhcp.lan.ra_flags='none'
+# del dhcp.lan.ra
+# del dhcp.lan.ra_slaac
+# del dhcp.lan.dhcpv6
+# commit dhcp
+# set system.@system[0]=system
+# set system.@system[0].hostname='CN210101841'
+# set system.@system[0].timezone='CST-8'
+# set system.@system[0].zonename='Asia/Shanghai'
+# set system.@system[0].ttylogin='0'
+# set system.@system[0].log_size='64'
+# set system.@system[0].log_proto='udp'
+# set system.@system[0].urandom_seed='0'
+# commit system
+# EOF
